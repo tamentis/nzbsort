f6363913ba0694dd509a6d7a149bb500ca222177
Bertrand Janin <tamentis@neopulsar.org>
2010-05-09 02:50:28 -0400

    Forgot to bump the version for setup.py =/

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="nzbsort",
-    version="0.1.0",
+    version="0.1.1",
     description="NZB files alpha sorter",
     author="Bertrand Janin",
     author_email="tamentis@neopulsar.org",
